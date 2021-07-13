48f25982b0e8ff2d3244b4d5184455439011e1da
Francisco <Francisco@DESKTOP-DM6H3SU>
2021-07-13 12:04:19 +0100

    inicial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React,{useState, useEffect}  from 'react';
+import { Data } from './DataTable';
 
+//http://movie-challenge-api-xpand.azurewebsites.net
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data/>
     </div>
   );
 }
